78f86977be9f7e9986fd85a84b14c5788213e979
root <root@SHAVYROVAJ10.by-minsk.CSI.local>
2017-05-30 12:24:08 +0300

    plugin edited

--- assets/js/jquery.combobox.js
+++ assets/js/jquery.combobox.js
@@ -1,61 +1,81 @@
 (function ( $ ) {
-    $(document).on('data-changed', function(event, oldData, newData) {
-        if (oldData !== newData) {
-            console.log('changed)');
-        }
-    });
     var methods = {
-        init : function(options) {
-            
+        init : function(settings) {
             return this.each(function(){
+                $(this).on('value-changed', function(event, oldValue, newValue) {
+                    if (oldValue !== newValue) {
+                        return settings.onValueChanged(oldValue, newValue);
+                    }
+                })
                 var $this = $(this),
-                    selectedValue = null,
-                    selectedIndex = 0,
-                    options = $this.find('option');
-                if ( ! data ) {
-                    $(this).data('combobox', {
-                        target : $this[0],
-                        selectedValue: selectedValue,
-                        selectedIndex: selectedIndex
-                    });
+                    selectedValue = settings.value;
+                for (var i = 0; i < settings.options.length; i++) {
+                    var option = settings.options[i];
+                    $this.append('<option>'+option+'</option>');
                 }
+                if (settings.value) {
+                    $this.find('option').val(
+                        function(index, x){
+                            if(parseInt(x) === settings.value) {
+                                $(this).attr('selected', 'selected');  
+                            }
+                            return x;
+                        }
+                    );
+                }
+                if (!data) {
+                    $this.data('combobox', {
+                        target : $this[0],
+                        selectedValue: selectedValue
+                    });
+                }     
                 var data = $this.data('combobox');
-                console.log(data);
-                //console.log(options);
-                var currTarget = data.target;
-                options.click(function(event) {
-                    var optionIndex = options.index(this);
-                    $(document).trigger('data-changed', [ data.selectedIndex, optionIndex ]);
-                    data.selectedIndex = optionIndex;
-                    //$this.selectedIndex = optionIndex;
-                    options.removeAttr('selected');
-                    $(this).attr('selected', 'selected');
-                    /*for (var i = 0; i < options.length; i++) {
-                        console.log(options[i].selected, options);
-                    }  */    
-                    console.log($this.data('combobox').selectedIndex);
-                    //console.log($this.find('option:selected'));
+                console.log('data', data);
+                console.log('settings', settings);
+                console.log('val', settings.value);
+                
+                var optionElems = $this.find('option');
+                optionElems.click(function(event) {
+                    var optionValue = parseInt($(this).val());
+                    $this.trigger('value-changed', [ data.selectedValue, optionValue ]);
+                    if (!$(this).prop('selected')) {
+                        optionElems.removeAttr('selected');
+                        $(this).attr('selected', 'selected');  
+                        data.selectedValue = optionValue;
+                    }
+                    console.log(data.selectedValue);
                 });
             });
         },
         getValue : function() {
             var $this = $(this),
                 data = $this.data('combobox');
-            //console.log("YYYY", data);
-            return data.selectedIndex;
+            return data.selectedValue;
         },
-        setValue : function(index) {
+        setValue : function(value) {
             return this.each(function(){
                 var $this = $(this),
                     data = $this.data('combobox'),
-                    options = $this.find('option');
-                $(document).trigger('data-changed', [ data.selectedIndex, index]);
-                //data.target.selectedIndex = num;
-                options.removeAttr('selected');
-                $(options[index]).attr('selected', 'selected');
-                data.selectedIndex = index;
-                //$this.selectedIndex = index;
-                console.log(data.selectedIndex);
+                    options = $this.find('option'),
+                    valueExist = false;
+                options.val(
+                    function(index, x){
+                        if(parseInt(x) === value) {
+                            valueExist = true;
+                            options.removeAttr('selected');
+                            $(this).attr('selected', 'selected');  
+                        }
+                        return x;
+                    }
+                );
+                if (valueExist) {
+                    $this.trigger('value-changed', [ data.selectedValue, value ]);
+                    data.selectedValue = value;
+                }
+                else {
+                    console.log('incorrect value');
+                }
+                console.log(data.selectedValue);
             });
         },
     };
@@ -74,4 +94,3 @@
  
 }( jQuery ));
 
-$('not-select.combobox').combobox();
